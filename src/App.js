3a542b64be39233d8a809d9ea896cb3a97a37ccb
Henry Macfarlane <hjemacfarlane@gmail.com>
2020-08-06 15:17:43 +0100

    favicon added

--- src/App.js
+++ src/App.js
@@ -14,17 +14,16 @@ function App() {
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
         <a className="navbar-brand js-scroll-trigger" href="#page-top">
           <span className="d-block d-lg-none">Henry Macfarlane</span>
-          <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={require("./assets/img/profile.jpg")} alt="" /></span>
+          <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={require("./assets/img/profile.jpg")} alt="img of henry" /></span>
         </a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav">
             <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#about">About</a></li>
-            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
-            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#education">Education</a></li>
             <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#education">Education</a></li>
+            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
             <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
-            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
           </ul>
         </div>
       </nav>
@@ -40,12 +39,45 @@ function App() {
             <div className="subheading mb-5">
               <a href="mailto:hjemacfarlane@gmail.com">hjemacfarlane@gmail.com</a>
             </div>
-            <p className="lead mb-5">I am a driven coder who has a keen eye for detail and I am excited to join and be part of the coding world. I have a natural curiosity in all things tech which is what attracted me to this line of work. My experience at Develop Me was extremely positive and I really enjoyed both the Front-end and Back- end parts of the course and in particular Laravel and React. I look forward to working with people who share my enthusiasm for coding.</p>
+            <p className="lead mb-5">I am a driven web developer who has a keen eye for detail and I am excited to join and be part of the Web Development world. I have a natural curiosity in all things tech which is what attracted me to this line of work. My experience at Develop Me was extremely positive and I really enjoyed both the Front-end and Back-end parts of the course and in particular Laravel and React. I look forward to working with people who share my enthusiasm for coding.</p>
             <div className="social-icons">
               <a className="social-icon" href="https://www.linkedin.com/in/henry-macfarlane-a811a116a/"><FontAwesomeIcon icon={["fab", "linkedin"]} size="lg" /></a>
               <a className="social-icon" href="https://github.com/GRANDMAC96"><FontAwesomeIcon icon={["fab", "github"]} size="lg" /></a>
               <a className="social-icon" href="https://www.facebook.com/henry.macfarlane.7/"><FontAwesomeIcon icon={["fab", "facebook"]} size="lg" /></a>
             </div>
+          </div>
+        </section>
+        <hr className="m-0" />
+        {/* Skills */}
+        <section className="resume-section" id="skills">
+          <div className="resume-section-content">
+            <h2 className="mb-5">Skills</h2>
+            <div className="subheading mb-3">Programming Languages/Frameworks</div>
+            <ul className="list-inline">
+              <li className="list-inline-item"><strong>HTML</strong></li>
+              <li className="list-inline-item"><strong>CSS</strong></li>
+              <li className="list-inline-item"><strong>Javascript</strong></li>
+              <li className="list-inline-item"><strong>React</strong></li>
+              <li className="list-inline-item"><strong>Redux</strong></li>
+              <li className="list-inline-item"><strong>PHP</strong></li>
+              <li className="list-inline-item"><strong>Laravel</strong></li>
+              <li className="list-inline-item"><strong>Blade</strong></li>
+              <li className="list-inline-item"><strong>SQL</strong></li>
+            </ul>
+            <div className="subheading mb-3">Tools/Other Skills</div>
+            <ul className="list-inline">
+              <li className="list-inline-item"><strong>Git</strong></li>
+              <li className="list-inline-item"><strong>Gulp</strong></li>
+              <li className="list-inline-item"><strong>Bootstrap</strong></li>
+              <li className="list-inline-item"><strong>TDD</strong></li>
+              <li className="list-inline-item"><strong>AWS</strong></li>
+              <li className="list-inline-item"><strong>NPM</strong></li>
+              <li className="list-inline-item"><strong>REST API</strong></li>
+              <li className="list-inline-item"><strong>Wireframing</strong></li>
+              <li className="list-inline-item"><strong>Teamwork</strong></li>
+              <li className="list-inline-item"><strong>Integrity</strong></li>
+              <li className="list-inline-item"><strong>Time Management</strong></li>
+            </ul>
           </div>
         </section>
         <hr className="m-0" />
@@ -107,45 +139,13 @@ function App() {
           </div>
         </section>
         <hr className="m-0" />
-        {/* Skills */}
-        <section className="resume-section" id="skills">
-          <div className="resume-section-content">
-            <h2 className="mb-5">Skills</h2>
-            <div className="subheading mb-3">Programming Languages/Frameworks</div>
-            <ul className="list-inline">
-              <li className="list-inline-item"><strong>HTML</strong></li>
-              <li className="list-inline-item"><strong>CSS</strong></li>
-              <li className="list-inline-item"><strong>Javascript</strong></li>
-              <li className="list-inline-item"><strong>React</strong></li>
-              <li className="list-inline-item"><strong>Redux</strong></li>
-              <li className="list-inline-item"><strong>PHP</strong></li>
-              <li className="list-inline-item"><strong>Laravel</strong></li>
-              <li className="list-inline-item"><strong>Blade</strong></li>
-              <li className="list-inline-item"><strong>SQL</strong></li>
-            </ul>
-            <div className="subheading mb-3">Tools/Other Skills</div>
-            <ul className="list-inline">
-              <li className="list-inline-item"><strong>Git</strong></li>
-              <li className="list-inline-item"><strong>Gulp</strong></li>
-              <li className="list-inline-item"><strong>Bootstrap</strong></li>
-              <li className="list-inline-item"><strong>TDD</strong></li>
-              <li className="list-inline-item"><strong>AWS</strong></li>
-              <li className="list-inline-item"><strong>NPM</strong></li>
-              <li className="list-inline-item"><strong>REST API</strong></li>
-              <li className="list-inline-item"><strong>Wireframing</strong></li>
-              <li className="list-inline-item"><strong>Teamwork</strong></li>
-              <li className="list-inline-item"><strong>Integrity</strong></li>
-              <li className="list-inline-item"><strong>Time Management</strong></li>
-            </ul>
-          </div>
-        </section>
-        <hr className="m-0" />
+
         {/* Interests */}
         <section className="resume-section" id="interests">
           <div className="resume-section-content">
             <h2 className="mb-5">Interests</h2>
-            <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
-            <p className="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
+            <p>Apart from being a web developer, I enjoy most of my time doing outdoors activities such as tennis and I run from time to time.</p>
+            <p className="mb-0">However when inside I do enjoy watching movies/TV shows and some ping pong too. </p>
           </div>
         </section>
         <hr className="m-0" />
