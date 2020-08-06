7f31928b83699cc74a6debd55a363723ce97238e
Henry Macfarlane <hjemacfarlane@gmail.com>
2020-08-06 17:35:03 +0100

    Updates

---
+++ precache-manifest.66d97099e32fa48745954abfe5547d0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84f5dcda82b6159352a3a816db738da",
+    "url": "/portfolio1/index.html"
+  },
+  {
+    "revision": "cae2ceca5a8b69ab2fb0",
+    "url": "/portfolio1/static/css/main.92e829d2.chunk.css"
+  },
+  {
+    "revision": "56fd5136f6fc3066c2c4",
+    "url": "/portfolio1/static/js/2.2ffd4125.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio1/static/js/2.2ffd4125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cae2ceca5a8b69ab2fb0",
+    "url": "/portfolio1/static/js/main.516e6e91.chunk.js"
+  },
+  {
+    "revision": "c98ff24afaa0a7d926fb",
+    "url": "/portfolio1/static/js/runtime-main.616a7c63.js"
+  },
+  {
+    "revision": "2fb0e8de8ae346ba9e91baa7e046c8a6",
+    "url": "/portfolio1/static/media/profile.2fb0e8de.jpg"
+  }
+]);
