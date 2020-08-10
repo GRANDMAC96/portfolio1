0acc515788c84cecb51e4f1fe980ee0940219b37
Henry Macfarlane <hjemacfarlane@gmail.com>
2020-08-10 11:22:40 +0100

    further favicon files added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/css/styles.css';
-// import 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
